2512c83008aa29aec9f048f461a90612b79b57c1
amandeepkaur <amandeepsandhu1822@gmail.com>
2023-02-09 20:29:36 -0500

    Initial Commit

---
+++ Amandeep_Kaur_MidTerm/Amandeep_Kaur_MidTermApp.swift
@@ -0,0 +1,17 @@
+//
+//  Amandeep_Kaur_MidTermApp.swift
+//  Amandeep_Kaur_MidTerm
+//
+//  Created by Amandeep Kaur on 2023-02-09.
+//
+
+import SwiftUI
+
+@main
+struct Amandeep_Kaur_MidTermApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Amandeep_Kaur_MidTerm/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Amandeep_Kaur_MidTerm
+//
+//  Created by Amandeep Kaur on 2023-02-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
